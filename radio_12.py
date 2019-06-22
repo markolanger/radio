bfdb6d794558733ea014dbc0caa00db419a69714
M <markolanger@users.noreply.github.com>
2019-06-22 13:39:14 +0200

    Update radio_12.py

--- radio_12.py
+++ radio_12.py
@@ -18,13 +18,13 @@ while running:
 
 	current_time = datetime.datetime.now().strftime('%d.%m.%Y')
 	current_clock = datetime.datetime.now().strftime('%H:%M:%S')
-	clock_label = title_font.render(current_clock, 1, (grey))
-	time_label = font.render(current_time, 1, (black))
-	station_name=station_font.render(line1, 1, (black))
-	additional_data=station_font.render(line2, 1, (blue))
-	station_label=font.render(station_status, 1, (status_font))
-	pygame.draw.rect(screen, (black), (0,62,820,42), 3)
-	pygame.draw.rect(screen, (black), (0,293,820,37), 1)
+	clock_label = font.render(current_clock, 1, (font_color))
+	time_label = font.render(current_time, 1, (font_color))
+	station_name=font.render(line1, 1, (font_color))
+	additional_data=font.render(line2, 1, (font_color))
+	station_label=font.render(station_status, 1, (font_color))
+	pygame.draw.rect(screen, (font_color), (0,62,820,42), 3)
+	pygame.draw.rect(screen, (font_color), (0,293,820,37), 1)
 	screen.blit(time_label,   (690, 300))
 	screen.blit(station_label,(255,300))
 	screen.blit(station_name,(260,66))
@@ -34,7 +34,7 @@ while running:
 	volume = subprocess.check_output("mpc volume", shell=True )
 	volume = volume[8:]
 	volume = volume[:-1]
-	volume_tag=font.render(volume, 1, (black))
+	volume_tag=font.render(volume, 1, (font_color))
 	screen.blit(volume_tag,(440,300))
   
    for event in pygame.event.get():
