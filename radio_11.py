4608e105fca1a6f67b3db55fbc7acd8098b239be
M <markolanger@users.noreply.github.com>
2019-06-22 10:24:44 +0200

    Update radio_11.py

--- radio_11.py
+++ radio_11.py
@@ -11,12 +11,13 @@ subprocess.call('mpc play' , shell=True)
 running = True
 
 def refresh_menu_screen():
-#set up the fixed items on the menu
+	print "refresh_menu_screen"
+	#set up the fixed items on the menu
 	screen.fill(white) #change the colours if needed
-	font=pygame.font.Font(None,15)
+	font=pygame.font.Font(None,20)
 	title_font=pygame.font.Font(None,15)
 	station_font=pygame.font.Font(None,15)
-###### display the station name and split it into 2 parts : 
+	###### display the station name and split it into 2 parts : 
 	station = subprocess.check_output("mpc current", shell=True )
 	lines=station.split(":")
 	length = len(lines) 
@@ -38,31 +39,23 @@ def refresh_menu_screen():
 	else:
 		station_status = "playing"
 		status_font = black
-
-
 	current_time = datetime.datetime.now().strftime('%H:%M:%S  %d.%m.%Y')
 	time_label = station_font.render(current_time, 1, (black))
-	
-	
 	station_name=station_font.render(line1, 1, (black))
 	additional_data=station_font.render(line2, 1, (blue))
-	
 	station_label=title_font.render(station_status, 1, (status_font))
 	pygame.draw.rect(screen, (black), (0,17,519,24), 2)
-	
 	screen.blit(time_label,   (360,  0))
 	screen.blit(station_label,(5,0))
 	screen.blit(station_name,(5,20))
 	screen.blit(additional_data,(5,30))
-	
-
-######## add volume number
+	######## add volume number
 	volume = subprocess.check_output("mpc volume", shell=True )
 	volume = volume[8:]
 	volume = volume[:-1]
 	volume_tag=font.render(volume, 1, (black))
 	screen.blit(volume_tag,(85,0))
-####### check to see if the Radio is connected to the internet
+	####### check to see if the Radio is connected to the internet
 	IP = subprocess.check_output("hostname -I", shell=True )
 	IP=IP[:3]
 	if IP =="192":
@@ -78,24 +71,27 @@ def refresh_menu_screen():
 	pygame.display.flip()
 
 def main():
+	print "main"
         while 1:
                 for event in pygame.event.get():
                         if event.type == pygame.MOUSEBUTTONDOWN:
-                                #print "screen pressed" #for debugging purposes
+                                print "screen pressed" #for debugging purposes
                                 pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
-                                #print pos #for checking
+                                print pos #for checking
                                 pygame.draw.circle(screen, white, pos, 2, 0) #for debugging purposes - adds a small dot where the screen is pressed
                                 on_click()
 
-#ensure there is always a safe way to end the program if the touch screen fails
+	#ensure there is always a safe way to end the program if the touch screen fails
                         if event.type == KEYDOWN:
                                 if event.key == K_ESCAPE:
-                                        sys.exit()
-        time.sleep(0.2)        
-	pygame.display.update()
-	#pygame.display.flip()
-	#refresh_menu_screen() 
-	#main()
+					sys.exit()
+			else:
+	        	print "3"
+			time.sleep(0.2)
+			pygame.display.update()
+			pygame.display.flip()
+			refresh_menu_screen()
+			#main()
 
 #################### EVERTHING HAS NOW BEEN DEFINED ###########################
 
@@ -111,6 +107,8 @@ white = 255, 255, 255
 yellow = 255, 255, 0
 red = 255, 0, 0
 green = 0, 255, 0
+print "1"
 refresh_menu_screen()  #refresh the menu interface 
+print "2"
 main() #check for key presses and start emergency exit
 #station_name()
